fe84e061e91b13352ed94e1bb2c4a44496358163
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-13 22:50:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				int s = 0;
+				Console.Write("Введите строку: ");
+				StringBuilder a = new StringBuilder(Console.ReadLine());
+				string str = a.ToString();
+				if (!String.IsNullOrWhiteSpace(str))
+				{
+					for (int i = 0; i < str.Length; i++)
+					{
+						if (str[i] == ':') s++;
+					}
+					if (s > 0)
+						Console.WriteLine("Символы после последнего двоеточия: " + str.Substring(str.LastIndexOf(':') + 1));
+					else Console.WriteLine("Двоеточие в предложении отсутствует!");
+				}
+				else
+				{
+					throw new Exception();
+				}
+			}
+			catch { Console.WriteLine("Некорректный ввод!"); }
+		}
+	}
+}
